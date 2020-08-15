96c383fc0e0817934c9ff9eeead45612bf70b0d4
beatmil <anutorn@gmail.com>
2020-08-16 01:06:03 +0700

    modified:   scene/fight.tscn 	modified:   script/ball.gd 	modified:   script/fight.gd         power_bar now linked with ball, each time click will spawn         another ball

--- script/ball.gd
+++ script/ball.gd
@@ -4,9 +4,7 @@ extends RigidBody2D
 
 # x axis power should be around 0 - 1000
 var POWER = Vector2(100,-500)
-var start_position = $".".global_position
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	
 #	set_gravity_scale(6)
@@ -23,11 +21,5 @@ func _integrate_forces(state):
 	
 func apply_force2(fire_power):
 	POWER.x = fire_power
-	$".".transform = Transform2D(0,Vector2(100,100))
-	$".".linear_velocity = Vector2.ZERO
-#	$".".position = Vector2(100,100)
 	apply_central_impulse(POWER)
 
-func _on_power_bar_fire(fire_power):
-	apply_force2(fire_power)
-	pass
--- script/fight.gd
+++ script/fight.gd
@@ -1,15 +1,12 @@
+# fight.cd
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var BALL = preload("res://prefabs/ball.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("maika")
-   
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,4 +14,10 @@ func _process(delta):
 	# always times delta
 	$dog.move_local_x(-10 * delta)
 	$cat.move_local_x(10 * delta)
-	pass
+
+
+func _on_power_bar_fire(fire_power):
+	var ball = BALL.instance()
+	ball.position = Vector2(480,750)
+	add_child(ball)
+	ball.apply_force2(fire_power)
