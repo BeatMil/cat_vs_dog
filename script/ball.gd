2d40638f4e2ca9f9228ef9cf6c709f345081545e
beatmil <anutorn@gmail.com>
2020-10-27 23:15:03 +0700

    I'm back to finish this game!! I mean like to create this gamea!  Changes to be committed: 	modified:   script/ball.gd 	modified:   script/fight.gd

--- script/ball.gd
+++ script/ball.gd
@@ -10,17 +10,12 @@ func _ready():
 	set_gravity_scale(6)
 	add_torque(5000)
 
-
-func _physics_process(delta):
-	pass
-
-func _integrate_forces(state):
-	pass
+# func _integrate_forces(state):
+	# pass
 	
 func apply_force2(fire_power):
 	POWER.x = fire_power
 	apply_central_impulse(POWER)
-
 
 
 func _on_VisibilityNotifier2D_screen_exited():
--- script/fight.gd
+++ script/fight.gd
@@ -7,6 +7,7 @@ var BALL = preload("res://prefabs/ball.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$power_bar/Button.grab_focus()
+	print("beat is the best!")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,6 +15,9 @@ func _process(delta):
 	# always times delta
 	$dog.move_local_x(-10 * delta)
 	$cat.move_local_x(10 * delta)
+
+func _physics_process(delta):
+	print($dog.get_position())
 
 
 func _on_power_bar_fire(fire_power):
