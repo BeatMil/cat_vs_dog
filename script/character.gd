74a6288dbdbd22d2521db45e0289f824f791fbe5
beatmil <anutorn@gmail.com>
2020-11-07 22:18:46 +0700

    created character which is an abstract class  that Ajarn tought me one time.  Changes to be committed: 	modified:   prefabs/ball.tscn 	new file:   prefabs/cat.tscn 	new file:   prefabs/character.tscn 	modified:   prefabs/dog.tscn 	modified:   project.godot 	modified:   scene/fight.tscn 	new file:   script/character.gd 	modified:   script/fight.gd

---
+++ script/character.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+var this_name = $".".get_name()
+
+
+func _ready():
+	pass
+
+
+func _on_head_body_entered(body):
+	if body.is_in_group("normal_attack"):
+		print("%s hits %s head"%[body,this_name])
+		$AnimationPlayer.play("hurt")
+
+
+
+func _on_body_body_entered(body):
+	if body.is_in_group("normal_attack"):
+		print("%s hits %s body"%[body,this_name])
+		$AnimationPlayer.play("hurt")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "hurt":
+		$AnimationPlayer.play("idle")
--- script/fight.gd
+++ script/fight.gd
@@ -6,13 +6,15 @@ var BALL = preload("res://prefabs/ball.tscn")
 
 func _ready():
 	$power_bar/Button.grab_focus()
-	print("beat is the best!")
+	$dog/AnimationPlayer.play("idle")
+	$cat/AnimationPlayer.play("idle")
+	$dog/sprite.set_flip_h(true)
 
 
 func _process(delta):
 	# always times delta
-	$dog.move_local_x(-10 * delta) # move dog
-	$cat.move_local_x(10 * delta) # move cat
+	# $dog.move_local_x(-10 * delta) # move dog
+	pass
 
 
 func _on_power_bar_fire(fire_power):
